a959d433ef88e478f0eb063251cdae968d4a3904
Marcin Chmylko <chmylkomarcin@gmail.com>
2025-04-10 16:16:50 +0300

    adding Log in test

--- cypress/e2e/LogIn.cy.js
+++ cypress/e2e/LogIn.cy.js
@@ -1,11 +1,22 @@
 
 
-const user = "user"
-const pass = "lala"
+const email = "marcinchmylko1234@gmail.com"
+const pass = "supertester1234"
 
 describe('User login', () => {
-  it('#1 User writes not existing data', () => {
+  it('#1 User writes wrong user credentials and error message appears', () => {
+
+    cy.viewport(1280, 800)
     cy.visit('https://rembeauty.com/')
+    cy.contains('button', 'accept all').click();
+    cy.get('button[aria-label="Close dialog"]').click();
+
     cy.get('a[aria-label="Open account"]').click()
+
+    cy.get('#CustomerEmail').type(email);            
+    cy.get('#CustomerPassword').type(pass);  
+    cy.contains('Sign In').scrollIntoView().click();
+
+    cy.url().should('eq', 'https://rembeauty.com/account');
   })
 })
