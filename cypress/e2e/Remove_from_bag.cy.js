c0206050054cc24544721da922a5213bc1493f3c
Marcin Chmylko <chmylkomarcin@gmail.com>
2025-04-11 14:39:53 +0300

    adding subscribe and bag remove tests

---
+++ cypress/e2e/Remove_from_bag.cy.js
@@ -0,0 +1,42 @@
+describe('Remove from bag', () => {
+  it('#1 User adds products to bag, removes all and validates nothing left in the bag', () => {
+
+    cy.viewport(1280, 800)
+    cy.visit('https://rembeauty.com/')
+    cy.contains('button', 'accept all').click();
+    cy.get('button[aria-label="Close dialog"]').click();
+
+    cy.get('a[aria-label="shop now"]').click()
+
+
+    cy.get('button.js-product-form-button.tvg_add_to_cart_button_wKPx5a_button').scrollIntoView().should('be.visible').click();
+
+
+     cy.get('.tvg_react_cart_F4VlJW_CartItems')
+     .should('not.be.empty') 
+     .find('.tvg_react_cart_F4VlJW_CartItem')
+     .should('have.length.greaterThan', 0); 
+
+
+   cy.get('.tvg_react_cart_F4VlJW_CartLineItemQtyRemove')
+     .first()  
+     .click();
+
+
+   cy.wait(1000);  
+
+
+   cy.get('.tvg_react_cart_F4VlJW_CartItems')
+     .should('be.empty'); 
+
+   cy.get('.tvg_react_cart_F4VlJW_CartEmpty')
+     .should('be.visible')
+     .find('.tvg_react_cart_F4VlJW_CartEmptyTitle')
+     .should('have.text', 'Your Bag is empty.');
+
+
+   cy.get('.tvg_react_cart_F4VlJW_CartEmptyButton')
+     .should('be.visible')
+     .and('have.text', 'Continue shopping');
+  })
+})
---
+++ cypress/e2e/Subscribe_succesful.cy.js
@@ -0,0 +1,21 @@
+describe("Klaviyo subscription confirmation", () => {
+  it("waits for 'Thanks for subscribing' message to appear", () => {
+    cy.viewport(1905, 578);
+    cy.visit("https://rembeauty.com/");
+
+    cy.contains('button', 'accept all').click();
+    cy.get('button[aria-label="Close dialog"]').click();
+
+    // Interact with the form
+    cy.get("form.js-signup-form").should("be.visible");
+    cy.get("#email").type("user@gmail.com", { delay: 50 });
+    cy.get("form.js-signup-form button[type='submit']")
+      .scrollIntoView()
+      .click({ force: true });
+
+    // Wait for success message using different selector approaches
+    cy.get("div.tvg_footer_jwX0la_Footer-subscribeForm > div > div > div", { timeout: 10000 })
+      .contains("Thanks for subscribing")
+      .should("be.visible");
+  });
+});
